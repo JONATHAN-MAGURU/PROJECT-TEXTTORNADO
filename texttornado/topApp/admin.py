bb71b41bee57d86294867f4631fe4e03a9ad7e76
jay clusters <jonathanmaguru@gmail.com>
2023-09-04 10:26:07 +0100

    committing to main

--- texttornado/topApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from topApp.models import Player, Comments, TypingDetails, TypingDetailsHistory
-# Register your models here.
-
-admin.site.register(Player)
-admin.site.register(Comments)
-admin.site.register(TypingDetails)
-admin.site.register(TypingDetailsHistory)
